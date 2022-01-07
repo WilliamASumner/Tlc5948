dec827f1fe09c4b28789e499fe4aa5d8dbd115a6
Will Sumner <williamasumner@gmail.com>
2022-01-07 10:35:16 -0500

    Add number of tlcs as member

--- Tlc5948.cpp
+++ Tlc5948.cpp
@@ -57,7 +57,7 @@ inline void copyBuf(void* inBuf, void* outBuf, unsigned int size) {
     }
 }
 
-void Tlc5948::writeControlBufferSPI(uint8_t numTlcs) {
+void Tlc5948::writeControlBufferSPI() {
     SPI.beginTransaction(SPISettings(TLC5948_SPI_SPEED,TLC5948_BIT_ORDER,TLC5948_SPI_MODE));
     if (numTlcs == 0)
         return;
@@ -84,7 +84,7 @@ void Tlc5948::writeControlBufferSPI(uint8_t numTlcs) {
 }
 
 // send data from ctrl buff
-void Tlc5948::writeControlBuffer(uint8_t numTlcs) {
+void Tlc5948::writeControlBuffer() {
     //ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     disableSPI();
     for (uint8_t i = 0; i < numTlcs; i++) {
@@ -98,13 +98,14 @@ void Tlc5948::writeControlBuffer(uint8_t numTlcs) {
 }
 
 // write to Gs buffer with SPI
-void Tlc5948::writeGsBufferSPI16(uint16_t* buf, uint16_t numVals, uint8_t numTlcs) { // buffer with correct endianness for 16bit values
+// Two args (number of Tlcs is calculated)
+void Tlc5948::writeGsBufferSPI16(uint16_t* buf, uint16_t numVals) { // buffer with correct endianness for 16bit values
     //ATOMIC_BLOCK(ATOMIC_RESTORESTATE) // make this function atomic
     if (numTlcs == 0 && numVals % 32 != 0) // don't accept weird input that would require padding
         return;
 
     SPI.beginTransaction(SPISettings(TLC5948_SPI_SPEED,TLC5948_BIT_ORDER,TLC5948_SPI_MODE));
-    if (numTlcs == 0) numTlcs = numVals / 32; // if just using numVals, have to calculate number of Tlcs
+    if (numTlcs == 0) numTlcs = numVals / 16; // if just using numVals, have to calculate number of Tlcs
     uint16_t valToWrite = 0x0;
     uint16_t currVal = 0;
     for (uint8_t i = numTlcs; i > 1; i--) {
@@ -126,7 +127,7 @@ void Tlc5948::writeGsBufferSPI16(uint16_t* buf, uint16_t numVals, uint8_t numTlc
 }
 
 // send data from gsdata buff
-void Tlc5948::writeGsBuffer16(uint16_t* buf, uint16_t numVals, uint8_t numTlcs) {
+void Tlc5948::writeGsBuffer16(uint16_t* buf, uint16_t numVals) {
     //ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     if (numTlcs == 0 && numVals % 32 != 0) // don't accept weird input that would require padding
         return;
@@ -219,7 +220,7 @@ void Tlc5948::writeGsBuffer16(uint16_t* buf, uint16_t numVals, uint8_t numTlcs) 
     return flags;
 }*/
 
-void Tlc5948::begin(bool usingSPI) {
+void Tlc5948::begin(bool usingSPI, uint8_t numberTlcs) {
     // Note: driver must first send gs + dc/bc/fctrl data before it will turn on
     // this function just gets the buffers ready, 2+ calls to writeData are needed
     // to actually start the chip
@@ -238,20 +239,25 @@ void Tlc5948::begin(bool usingSPI) {
     setDcData(Channels::all,0x7f); // all dot correction to 100%
     setBcData(0x7f); // global brightness to max
 
-    Fctrls funcControls =   Fctrls::blank_mode_0    | // blank is set to 1 by chip, need to zero it out to use chip
+    Fctrls funcControls =   Fctrls::blank_mode_0    | // needs to be 0 to use chip
                             Fctrls::dsprpt_mode_0   | // no async update color data
                             Fctrls::tmgrst_mode_0   | // no timing rst, lat is async
                             Fctrls::espwm_mode_0    | // no ES_PWM
                             Fctrls::lodvlt_mode_12v | // highest LOD vlt (1.2V)
                             Fctrls::lsdvlt_mode_065 | // highest LSD vlt (0.65*vcc)
-                            Fctrls::lattmg_mode_17  | // 17 clks before we can latch error data
+                            Fctrls::lattmg_mode_17  | // 17 clks then we can latch error data
+                            Fctrls::idmena_mode_0   | // no idm 0
+                            Fctrls::idmrpt_mode_0   | // no idm 0
                             Fctrls::idmcur_mode_2ua | // 2ua on IDM
+                            Fctrls::oldena_mode_0   | // 2ua on IDM
                             Fctrls::psmode_none;      // no power saving mode
                             //Fctrls::psmode_sclk     | // power off until sclk
                             //Fctrls::psmode_data     | // power off until new data
                             //Fctrls::psmode_noclk; // turn off internal GSCLK on power save mode
 
     setFctrlBits(funcControls);
+
+    numTlcs = numberTlcs; // set up number of tlcs we're using
 }
 
 void Tlc5948::readDeviceContents(uint8_t *bytes, int numBytes) {
